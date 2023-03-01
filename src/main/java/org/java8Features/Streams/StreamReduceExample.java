e3ff9e87c2ccadfe5e49e48b6d7db2fd61f9aa73
tejas-coditas <tejas.kale@coditas.com>
2023-03-01 11:08:43 +0530

    java8 features: reduce without identity

--- src/main/java/org/java8Features/Streams/StreamReduceExample.java
+++ src/main/java/org/java8Features/Streams/StreamReduceExample.java
@@ -15,14 +15,20 @@ public class StreamReduceExample {
                 //a=1,b=3 => result is 3
                 //a=3,b=5 => result 15
                 //a=15,b=7 => 105
-         reduce(1,(a,b)-> a*b);
+         reduce(2,(a,b)-> a*b);
+    }
+
+    static Optional<Integer> performMultiplicationWithoutIdentity(List<Integer> integerList){
+        return integerList.stream().reduce((a,b)->a*b);
     }
 
     public static void main(String[] args) {
         List<Integer> integers = Arrays.asList(1,3,5,7);
-        System.out.println("Multiplication: " + performMultiplication(integers));
+        System.out.println("\nMultiplication: " + performMultiplication(integers));
 
-        Optional<Integer> integer = integers.stream().reduce((a, b) ->a+b);
+        Optional<Integer> integer = performMultiplicationWithoutIdentity(integers);
+        System.out.println("\nMultiplication value present: " + integer.isPresent());
+        System.out.println("Multiplication without Identity: " + integer.get());
 
     }
 }
